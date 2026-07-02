b252016e005337282f7102f3131a06d3f717c857
zofan5tt542-sudo <zofan5tt542@gmail.com>
2026-07-02 23:55:22 +0300

    Update EnvVarsView.java

--- app/src/main/java/com/winlator/cmod/widget/EnvVarsView.java
+++ app/src/main/java/com/winlator/cmod/widget/EnvVarsView.java
@@ -44,7 +44,8 @@ public class EnvVarsView extends FrameLayout {
         {"WINE_DO_NOT_CREATE_DXGI_DEVICE_MANAGER", "CHECKBOX", "0", "1"},
         {"WINE_NEW_MEDIASOURCE", "CHECKBOX", "0", "1"},
         {"GALLIUM_HUD", "SELECT_MULTIPLE", "simple", "fps", "frametime"},
-        {"WINE_LARGE_ADDRESS_AWARE", "CHECKBOX", "0", "1"}
+        {"WINE_LARGE_ADDRESS_AWARE", "CHECKBOX", "0", "1"},
+        {"WINEDLLOVERRIDES", "TEXT"}
     };
     private final LinearLayout container;
     private final TextView emptyTextView;
