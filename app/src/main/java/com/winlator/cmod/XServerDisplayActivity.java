a5bd0d95dd76ce2a88a25a659277a664efa54747
sultan3554 <144125770+sultan3554@users.noreply.github.com>
2026-07-08 17:57:53 +0300

    Update XServerDisplayActivity.java

--- app/src/main/java/com/winlator/cmod/XServerDisplayActivity.java
+++ app/src/main/java/com/winlator/cmod/XServerDisplayActivity.java
@@ -5,6 +5,8 @@ import static com.winlator.cmod.core.AppUtils.showToast;
 import android.Manifest;
 import android.annotation.SuppressLint;
 import android.app.Activity;
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
 import android.app.PendingIntent;
 import android.content.Intent;
 import android.content.SharedPreferences;
@@ -39,6 +41,8 @@ import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.ActivityCompat;
+import androidx.core.app.NotificationCompat;
+import androidx.core.app.NotificationManagerCompat;
 import androidx.core.content.ContextCompat;
 import androidx.core.view.GravityCompat;
 import androidx.drawerlayout.widget.DrawerLayout;
@@ -130,6 +134,8 @@ import java.util.regex.Pattern;
 import cn.sherlock.com.sun.media.sound.SF2Soundbank;
 
 public class XServerDisplayActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
+    public static String NOTIFICATION_CHANNEL_ID = "Winlator";
+    public static int NOTIFICATION_ID = -1;
     private XServerView xServerView;
     private InputControlsView inputControlsView;
     private TouchpadView touchpadView;
@@ -198,6 +204,16 @@ public class XServerDisplayActivity extends AppCompatActivity implements Navigat
 
     private GuestProgramLauncherComponent guestProgramLauncherComponent;
     private EnvVars overrideEnvVars;
+
+    private void createNotifcationChannel() {
+        String name = "Winlator";
+        String description = "Winlator XServer Messages";
+        int importance = NotificationManager.IMPORTANCE_HIGH;
+        NotificationChannel channel = new NotificationChannel(NOTIFICATION_CHANNEL_ID, name, importance);
+        channel.setDescription(description);
+        NotificationManager notificationManager = getSystemService(NotificationManager.class);
+        notificationManager.createNotificationChannel(channel);
+    }
 
     @Override
     public void onConfigurationChanged(@NonNull Configuration newConfig) {
@@ -430,7 +446,7 @@ public class XServerDisplayActivity extends AppCompatActivity implements Navigat
         String wmClass = shortcut != null ? shortcut.getExtra("wmClass", "") : "";
         Log.d("XServerDisplayActivity", "Startup wmClass: " + wmClass);
 
-        firstTimeBoot = container.getExtra("imgVersion").isEmpty();
+        firstTimeBoot = container.getExtra("appVersion").isEmpty();
 
         String wineVersion = container.getWineVersion();
         wineInfo = WineInfo.fromIdentifier(this, contentsManager, wineVersion);
@@ -564,6 +580,20 @@ public class XServerDisplayActivity extends AppCompatActivity implements Navigat
 
         // Check if a profile is defined by the shortcut
         String controlsProfile = shortcut != null ? shortcut.getExtra("controlsProfile", "") : "";
+
+        createNotifcationChannel();
+
+        Intent notificationIntent = new Intent(this, XServerDisplayActivity.class);
+        PendingIntent pendingIntent = PendingIntent.getActivity(this, 0, notificationIntent, PendingIntent.FLAG_IMMUTABLE);
+        NotificationCompat.Builder builder = new NotificationCompat.Builder(this, NOTIFICATION_CHANNEL_ID)
+                .setSmallIcon(R.drawable.ic_stat_ab_gear_0011)
+                .setContentTitle("Winlator")
+                .setContentText("Winlator is running, do not kill or swipe this notification")
+                .setPriority(NotificationCompat.PRIORITY_HIGH)
+                .setContentIntent(pendingIntent)
+                .setAutoCancel(false);
+
+        NotificationManagerCompat.from(this).notify(NOTIFICATION_ID, builder.build());
 
         Runnable runnable = () -> {
             setupUI();
@@ -721,8 +751,7 @@ public class XServerDisplayActivity extends AppCompatActivity implements Navigat
         }
         startTime = System.currentTimeMillis();
         handler.postDelayed(savePlaytimeRunnable, SAVE_INTERVAL_MS);
-        if (!isInPictureInPictureMode())
-        	ProcessHelper.resumeAllWineProcesses();
+        ProcessHelper.resumeAllWineProcesses();
     }
 
     @Override
@@ -742,11 +771,11 @@ public class XServerDisplayActivity extends AppCompatActivity implements Navigat
                 environment.onPause();
                 xServerView.onPause();
             }
-			ProcessHelper.pauseAllWineProcesses();
         }
 
         savePlaytimeData();
         handler.removeCallbacks(savePlaytimeRunnable);
+        ProcessHelper.pauseAllWineProcesses();
     }
 
 
@@ -781,6 +810,7 @@ public class XServerDisplayActivity extends AppCompatActivity implements Navigat
     }
 
     private void exit() {
+        NotificationManagerCompat.from(this).cancel(NOTIFICATION_ID);
         preloaderDialog.showOnUiThread(R.string.shutdown);
         handler.postDelayed(new Runnable() {
             @Override
@@ -950,11 +980,13 @@ public class XServerDisplayActivity extends AppCompatActivity implements Navigat
     }
 
     private void setupWineSystemFiles() {
+        String appVersion = String.valueOf(AppUtils.getVersionCode(this));
         String imgVersion = String.valueOf(imageFs.getVersion());
         boolean containerDataChanged = false;
 
-        if (!container.getExtra("imgVersion").equals(imgVersion)) {
+        if (!container.getExtra("appVersion").equals(appVersion) || !container.getExtra("imgVersion").equals(imgVersion)) {
             applyGeneralPatches(container);
+            container.putExtra("appVersion", appVersion);
             container.putExtra("imgVersion", imgVersion);
             containerDataChanged = true;
         }
@@ -1942,7 +1974,6 @@ public class XServerDisplayActivity extends AppCompatActivity implements Navigat
     }
 
 }
-
 
 
 
